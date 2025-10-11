7e17919bdb3e2261366e8a7490c95d1e5003bf84
d-korkelia <datunatunia@gmail.com>
2025-10-11 20:12:53 +0400

    “commit”

--- day 00/classwork/cw.py
+++ day 00/classwork/cw.py
@@ -1,1 +1,15 @@
-#no classwork
+print("Datuna")
+my_name = "Datuna"
+print(my_name)
+my_number = 6
+print(my_number)
+
+
+item_price = 99.99
+print(item_price)
+city = "Tbilisi"
+print(city)
+age = 16
+print(age)
+name="DATUNA"
+print(name)
