1f0a00fe520a38d90b89170ce1e93150d2a6aa42
d-korkelia <datunatunia@gmail.com>
2025-11-16 14:26:37 +0400

    “commit”

---
+++ day 07/classwork/cw.py
@@ -0,0 +1,22 @@
+
+fruit = ["strawberries","pineapple","apple","blueberries", "banana", ]
+print(fruit[2:5])
+
+# Mutable ობიექტები:
+# ასეთ ობიექტებს შეცვლა შეიძლება პირდაპირ ახალი ობიექტის შექმნის გარეშე.
+# Immutable ობიექტები:
+# ასეთ ობიექტებს ვერ შევუცვლით მნიშვნელობას პირდაპირ.
+# mutable  შეიცვლება ადგილზე 
+# immutable თითოეულ ცვლილებაზე იქმნება ახალი ობიექტი
+
+name = "yone"
+print(name)
+
+first_name = "YONE"
+last_name = "Shoutstreet"
+age = 22
+
+person = [first_name, last_name, age]
+
+print( person)
+print(type(person))
